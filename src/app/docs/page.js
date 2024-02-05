18ba3aabb039c8c534670e0e9be6253351e08a35
sire <robertcxlee@gmail.com>
2024-02-04 20:19:59 -0700

    removed admin panel (sry hao)

--- src/app/docs/page.js
+++ src/app/docs/page.js
@@ -1,7 +1,7 @@
-"use client";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-    window.location.href = "/docs/installation";
-    
-    return <></>
+	redirect("/docs/installation");
+
+	return <></>;
 }
