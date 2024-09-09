9dbfebfa13f255c16e4061eb1b94d4dd3f3f3657
sireButItsUnique <robertcxlee@gmail.com>
2024-09-09 17:50:42 -0600

    added swapping for exemplars

--- web/src/app/exemplars/[exemplarId]/page.js
+++ web/src/app/exemplars/[exemplarId]/page.js
@@ -10,21 +10,30 @@ import "prismjs/components/prism-cpp";
 
 export default function Home() {
 	const url = usePathname();
-	const [content, setContent] = useState("# Loading...");
+	const [header, setHeader] = useState("# Loading...");
 	const [code, setCode] = useState("...");
-	const mdPath = `/data${url}.mdx`;
-	const codePath = `/data${url}.cpp`;
+	const [commentary, setCommentary] = useState("...");
+	const headerPath = `/data${url}/header.mdx`;
+	const commentaryPath = `/data${url}/commentary.mdx`;
+	const codePath = `/data${url}/code.cpp`;
+	const [showCode, setShowCode] = useState(false);
 
 	useEffect(() => {
-		fetch(mdPath)
+		fetch(headerPath)
 			.then((res) => res.text())
 			.then((text) => {
 				let regex = new RegExp("__next_error__", "i");
 				if (text.match(regex)) {
-					setContent("# Page doesn't exist");
+					setHeader("# Page doesn't exist");
 				} else {
-					setContent(text);
+					setHeader(text);
 				}
+			});
+		fetch(commentaryPath)
+			.then((res) => res.text())
+			.then((text) => {
+				setCommentary(text);
+				Prism.highlightAll();
 			});
 		fetch(codePath)
 		.then((res) => res.text())
@@ -37,14 +46,33 @@ export default function Home() {
 
 	return (
 		<section className="pt-14 pb-12 pl-[24.5rem] pr-20">
-			<div className="text-left">
-				<ReactMarkdown className="markdown" children={content} />
+		
+			<div className="inline flex justify-between">
+				<div className="text-left">
+					<ReactMarkdown className="markdown" children={header} />
+				</div>
+				<div className="justify-end flex-none content-center">
+					<button className={`${showCode ? "swap": ""} material-bubble w-full mb-[1rem] text-text-header block border border-text-header rounded py-1.5 px-3`} onClick={() => {
+						setShowCode(false);
+					}}>
+						Commentary
+					</button>
+					<button className={`${showCode ? "swapRev": ""} material-bubble w-full mt-[1rem] text-text-header block border border-text-header rounded py-1.5 px-3`} onClick={() => {
+						setShowCode(true);
+					}}>
+						Code
+					</button>
+				</div>
 			</div>
-			<pre class="language-cpp line-numbers" >
-				<code class="language-cpp">
+			
+			{!showCode && <div className="text-left">
+				<ReactMarkdown className="markdown" children={commentary} />
+			</div>}
+			{showCode && <pre className="language-cpp border-none" >
+				<code className="language-cpp border-none">
 					{code}
 				</code>
-			</pre>
+			</pre>}
 		</section>
 	);
 }
--- web/src/components/blur/blur.js
+++ web/src/components/blur/blur.js
@@ -4,43 +4,49 @@ export default function Blur() {
     return (
     <>
         <Image
-            className={`fixed blur-[2rem] right-[5rem] top-[5rem] z-40`}
+            className={`fixed blur-[3rem] right-[5rem] top-[5rem] z-[-10]`}
+            src="/images/logo.png"
+            width="150"
+            height="150"
+        />
+        <Image
+            className={`fixed blur-[2.5rem] left-[18rem] bottom-[4rem] z-[-10]`}
+            src="/images/logo.png"
+            width="100"
+            height="100"
+        />
+        <Image
+            className={`fixed blur-[2rem] right-[50rem] top-[25rem] z-[-10]`}
             src="/images/logo.png"
             width="50"
             height="50"
         />
         <Image
-            className={`fixed blur-[2rem] right-[50rem] top-[25rem] z-40`}
+            className={`fixed blur-[2rem] right-[35rem] top-[35rem] z-[-10]`}
             src="/images/logo.png"
             width="50"
             height="50"
         />
         <Image
-            className={`fixed blur-[2rem] right-[35rem] top-[35rem] z-40`}
+            className={`fixed blur-[2rem] right-[80rem] top-[25rem] z-[-10]`}
             src="/images/logo.png"
             width="50"
             height="50"
         />
         <Image
-            className={`fixed blur-[2rem] right-[80rem] top-[25rem] z-40`}
+            className={`fixed blur-[2rem] right-[75rem] top-[8rem] z-[-10]`}
             src="/images/logo.png"
             width="50"
             height="50"
         />
         <Image
-            className={`fixed blur-[2rem] right-[75rem] top-[8rem] z-40`}
+            className={`fixed blur-[2rem] right-[25rem] top-[12rem] z-[-10]`}
             src="/images/logo.png"
             width="50"
             height="50"
         />
         <Image
-            className={`fixed blur-[2rem] right-[25rem] top-[12rem] z-40`}
-            src="/images/logo.png"
-            width="50"
-            height="50"
-        />
-        <Image
-            className={`fixed blur-[2rem] right-[8rem] bottom-[4rem] z-40`}
+            className={`fixed blur-[2rem] right-[8rem] bottom-[4rem] z-[-10]`}
             src="/images/logo.png"
             width="50"
             height="50"
